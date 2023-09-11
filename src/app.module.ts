be3bc4142d5e57ce99a39c8759c955b78bd04fd9
지훈 서 <tjwlgns92@naver.com>
2023-09-11 17:03:00 +0900

    DB connection 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://ByeNolan:ByeNolan@byenolan.sje6f0n.mongodb.net/',
+    ),
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
